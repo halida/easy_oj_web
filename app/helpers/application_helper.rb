9d4ba7563ef948b5e652a2f842b406ef8e5d91cb
halida <linjunhalida@gmail.com>
2013-03-02 15:46:36 +0800

    better solution status ui

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def solution_status_display status
+    show = case status
+           when 'Accepted' then
+             'accepted'
+           when 'Pending' then
+             'pending'
+           else
+             'failed'
+           end
+    "<div class='solution_status #{show}'></div>".html_safe
+  end
 end
